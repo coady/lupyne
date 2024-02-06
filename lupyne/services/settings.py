7a186c2d55f0a8c5831d062cb23ac26eabc24c37
Aric Coady <aric.coady@gmail.com>
2024-02-05 17:32:10 -0800

    Starlette >=0.36 compatibility.

--- lupyne/services/settings.py
+++ lupyne/services/settings.py
@@ -1,7 +1,8 @@
+from pathlib import Path
 from starlette.config import Config
 from starlette.datastructures import CommaSeparatedStrings
 
-config = Config('.env')
+config = Config('.env' if Path('.env').is_file() else None)
 DEBUG = config('DEBUG', cast=bool, default=False)
 DIRECTORIES = config('DIRECTORIES', cast=CommaSeparatedStrings)
 SCHEMA = config('SCHEMA', default='')
